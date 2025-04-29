0f2c331e131102a84106764503af8325aa39dcde
Liz Posusta <lposusta01@m-mpx03qqk4c.admin.bvsd.k12.co.us>
2025-04-29 15:54:07 -0600

    silly

--- src/WeatherVis.java
+++ src/WeatherVis.java
@@ -1,5 +1,10 @@
 import javax.imageio.*;
+import java.io.*;
+import jakarta.json.*;
+import jakarta.json.stream.*;
 import java.awt.*; //i dont know if all of awt is necessary here but wtv
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 
 //rain %chance/temp = raindrop width, color (higher chance means brighten and desaturate)
 //rain %chance = raindrop particle intensity
@@ -48,21 +53,78 @@ public class WeatherVis {
 
     public class JSONdata {
         public int temp;
-        public int precipChance;
-        public int rainTempValue;
         public int windSpeed;
+        public int precipChance;
+
+        public JSONdata(int temp, int windSpeed, int precipChance) {
+            this.temp = temp;
+            this.windSpeed = windSpeed;
+            this.precipChance = precipChance;
+        }
+
     }
 
-    public static JSONdata generate_json_data(String input) {
-        JsonParser parser = new JsonParser(input);
+    enum SpecialParserState {
+        INIT,
+        READ_TEMP,
+        READ_WIND_SPEED,
+        READ_PRECIP_CHANCE,
+    } //i probably don't need to use a state machine for this but whatever
 
+    public JSONdata generate_json_data(String input) {
+        int temp = 0;
+        int windSpeed = 0;
+        int precipChance = 0;
+        JSONdata out = new JSONdata(temp, windSpeed, precipChance);
+        SpecialParserState state = SpecialParserState.INIT;
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+        JsonParser parser = Json.createParser(inputStream);
+        while(parser.hasNext()) {
+            if(parser.currentEvent() == JsonParser.Event.KEY_NAME) {
+                if(parser.getString() == "temperature") {
+                    state = SpecialParserState.READ_TEMP;
+                    parser.next();
+                } else if(parser.getString() == "windSpeed") {
+                    state = SpecialParserState.READ_WIND_SPEED;
+                    parser.next();
+                } else if(parser.getString() == "probabilityOfPrecipitation") {
+                    state = SpecialParserState.READ_PRECIP_CHANCE;
+                    parser.next();
+                } else {
+                    state = SpecialParserState.INIT;
+                    parser.next();
+                }
+            } else if(parser.currentEvent() == JsonParser.Event.VALUE_NUMBER) {
+                if(state != SpecialParserState.INIT) {
+                    switch(state) {
+                        case READ_TEMP:
+                            temp = parser.getInt();
+                            break;
+                        case READ_WIND_SPEED:
+                            temp = parser.getInt();
+                            break;
+                        case READ_PRECIP_CHANCE:
+                            temp = parser.getInt();
+                            break;
+                        default:
+                            System.err.println("Invalid parser state. What?????");
+                            break;
+                    }
+                } else {
+                    parser.next();
+                }
+            } else {
+                parser.next();
+            }
+        }
+        return out;
     }
 
-    public static RaindropParticle[] generate_raindrops(JSONdata data) {
+    //public static RaindropParticle[] generate_raindrops(JSONdata data) {
 
-    }
+    //}
 
     public static void main(String[] args) {
-
+        
     }
 }
